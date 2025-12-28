c94b6b04fe8fe468fa7949ee5d88676d6a6297ec
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-28 23:21:05 +0700

    Update keliling_dan_jari_jari_lingkaran.js

--- keliling_dan_jari_jari_lingkaran.js
+++ keliling_dan_jari_jari_lingkaran.js
@@ -1,18 +1,26 @@
-let jari_jari = 8;
-let phi = 0;
 
-luas = phi * jari_jari * jari_jari;
-keliling = 2 * phi * jari_jari;
+process.stdin.setEncoding('utf8');
 
-if (jari_jari % 7 == 0) {
+console.log("Masukkan jari-jari lingkaran:");
+process.stdin.on('data', (input) => {
+  const data = input.trim();
+  let jari_jari = parseFloat(data);
+
+  let phi;
+  if (jari_jari % 7 == 0) {
     phi = 22/7;
-} else {
+  } else {
     phi = 3.14;
-}
+  }
 
-luas_lingkaran = phi * jari_jari * jari_jari;
-console.log("luas lingkaran adalah " + luas_lingkaran); 
-
-keliling_lingkaran = 2 * phi * jari_jari;
-console.log("keliling lingkaran adalah " + keliling_lingkaran);
-
+  let luas_lingkaran = phi * jari_jari * jari_jari;
+  let keliling_lingkaran = 2 * phi * jari_jari;
+  
+  console.log("\n=== Hasil Perhitungan ===");
+  console.log("Jari-jari: " + jari_jari);
+  console.log("Nilai phi: " + phi);
+  console.log("Luas lingkaran adalah " + luas_lingkaran.toFixed(2));
+  console.log("Keliling lingkaran adalah " + keliling_lingkaran.toFixed(2));
+  
+  process.exit(0);
+});
